bd90326b0a9a30fc895b3297193ded303d143f7c
chof747 <chof@gmx.at>
2021-10-12 22:03:42 +0200

    Changed script to also deal with movies and issues
    - adjustname.sh is renaming now movies
    - images without createdate will be renamed consecutively after the images with tag
    - duplicates are renamed as duplicates
    
    Changes to setup test script and gitignore

--- adjustname.sh
+++ adjustname.sh
@@ -8,7 +8,14 @@ if test -n "$(find ${workdir} -maxdepth 1 -name '*.JPG' -print -quit)"; then
     rename 's/(.*)\.jpeg$/$1.jpg/' ${workdir}*.jpeg
 fi
 
-exiftool "-filename<DateTimeOriginal" -d "renaming_##%Y%m%d%H%M%S##.%%e" $workdir
+if test -n "$(find ${workdir} -maxdepth 1 -name '*.MOV' -print -quit)"; then
+    rename 's/(.*)\.MOV$/$1.movie/' ${workdir}*.MOV
+    rename 's/(.*)\.movie$/$1.mov/' ${workdir}*.movie
+fi
+
+
+exiftool "-filename<DateTimeOriginal" -d "renaming_##%Y%m%d%H%M%S##.%%e" $workdir/*.jpg
+exiftool "-filename<DateTimeOriginal" -d "duplicate_##%Y%m%d%H%M%S##%%-c.%%e" $workdir/IMG*.jpg
 
 countjpg=$((`find ${workdir} -maxdepth 1 -type f -name *.jpg | wc -l`))
 
@@ -23,3 +30,19 @@ for img in `ls ${workdir}renaming*.jpg`; do
     mv $img `printf "${workdir}${album}_%0${digits}d.jpg" $i`
     ((i=i+1))
 done    
+
+if test -n "$(find ${workdir} -maxdepth 1 -name 'IMG*.jpg' -print -quit)"; then
+    for img in `ls ${workdir}IMG*.jpg`; do
+        mv $img `printf "${workdir}${album}_%0${digits}d.jpg" $i`
+        ((i=i+1))
+    done    
+fi
+
+
+if test -n "$(find ${workdir} -maxdepth 1 -name '*.mov' -print -quit)"; then
+    i=1
+    for mov in `ls ${workdir}*.mov`; do
+        mv $mov `printf "${workdir}${album}_m%02d.mov" $i`
+        ((i=i+1))
+    done    
+fi
--- test/setup.sh
+++ test/setup.sh
@@ -3,6 +3,6 @@ TEST_DIR=`dirname $0`
 TEST_DIR="${TEST_DIR}/fotos"
 
 echo $TEST_DIR
-[[ -d "$TEST_DIR/20210806_lesung-semmering" ]] && rm -rf "$TEST_DIR/20210806_lesung-semmering"
+[[ -d "$TEST_DIR/20150118_koeszeg" ]] && rm -rf "$TEST_DIR/20150118_koeszeg"
 mkdir -p $TEST_DIR
-cp -R /Volumes/fotos/2021/20210806_lesung-semmering $TEST_DIR
+cp -R /Volumes/fotos/2015/20150118_koeszeg $TEST_DIR
