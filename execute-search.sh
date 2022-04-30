3abdb6fff2ad427d398e97fc2be4c91f514b146a
chof747 <chof@gmx.at>
2022-04-30 23:01:42 +0200

    Add recycle path to the ignores

--- execute-search.sh
+++ execute-search.sh
@@ -2,7 +2,7 @@
 
 find $FOTOS_PATH -type f |
  grep -vE ".*/[0-9]{8}_[a-zA-Z-]*?_([0-9]*\.jpg|m[0-9]*\.mov)" |
- grep -vE "\.ini|\.db|\.info" |
+ grep -vE "\.ini|\.db|\.info|@Recycle" |
  grep -E ".*?/[0-9]{8}_[a-zA-Z-]*/[^/]*\.[[:alnum:]]{3,4}" | 
  sed -n -e 's/\(.*[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]_[a-zA-Z0-9-]*\)\/.*/\1/p' |
  sort | uniq |
