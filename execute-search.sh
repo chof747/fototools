a24b925d2bc5fb58034b831250e19754fb51e7c2
Christian Hofbauer <chof@gmx.at>
2023-06-23 21:02:02 +0200

    Added comment to execute search

--- execute-search.sh
+++ execute-search.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
 
 find $FOTOS_PATH -type f |
+ # regexp to exclude the already correctly formatted names of images and movies
  grep -vE ".*/[0-9]{8}_[a-zA-Z-]*?_([0-9]*\.jpg|m[0-9]*\.mov)" |
+ # regexp to exclude special files
  grep -vE "\.ini|\.db|\.info|@Recycle" |
+ #select all entries that are in correctly named folders and have a 3-4 digit file extension
  grep -E ".*?/[0-9]{8}_[a-zA-Z-]*/[^/]*\.[[:alnum:]]{3,4}" | 
+ #extract only the path part
  sed -n -e 's/\(.*[0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]_[a-zA-Z0-9-]*\)\/.*/\1/p' |
  sort | uniq |
     while read folder ; do
